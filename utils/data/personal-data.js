668e6f6a4c457658815ce8759703c0f77fddc062
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-04-17 12:09:54 +0530

    added the hearo section

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
-  name: "Saheb Ansari",
-  profile: '/profile.png',
-  designation: "Android  Developer",
-  description: "Hello, I'm Saheb Ansari, an IT Junior at Chandigarh Engineering College, CGC Landran, with expertise in Android development using Core Java, Kotlin, and Flutter. My skills extend to cloud engineering, where I manage Kubernetes, Docker, and CI/CD practices for robust cloud environments. I am ranked 17th globally and 3rd nationwide in the Google Cloud Ready Facilitator Program 2022 and have actively contributed to open-source projects and Hacktoberfest.",
-  email: 'sahebansari786921@gmail.com',
-  phone: '+918102685094',
-  address: 'Chandigarh , Punjab',
-  github: 'https://github.com/saheb9631',
-  facebook: 'https://www.facebook.com/',
-  linkedIn: 'https://www.linkedin.com/in/saheb-ansari-/',
-  twitter: 'https://twitter.com/',
-  stackOverflow: 'https://stackoverflow.com/',
+  name: "Roshan",
+  profile: "/profile.png",
+  designation: "Software Developer",
+  description:
+    "Hello, I'm Saheb Ansari, an IT Junior at Chandigarh Engineering College, CGC Landran, with expertise in Android development using Core Java, Kotlin, and Flutter. My skills extend to cloud engineering, where I manage Kubernetes, Docker, and CI/CD practices for robust cloud environments. I am ranked 17th globally and 3rd nationwide in the Google Cloud Ready Facilitator Program 2022 and have actively contributed to open-source projects and Hacktoberfest.",
+  email: "kumarros2002@gmail.com",
+  phone: "+918851504771",
+  address: "New Delhi , Delhi",
+  github: "https://github.com/saheb9631",
+  facebook: "https://www.facebook.com/",
+  linkedIn: "https://www.linkedin.com/in/saheb-ansari-/",
+  twitter: "https://twitter.com/",
+  stackOverflow: "https://stackoverflow.com/",
   leetcode: "https://leetcode.com/saheb9631/",
   devUsername: "saheb9631",
-  resume: "https://drive.google.com/file/d/1q-3jZ8f8plGZi8WA0o4F7MomaC-CfXfl/view?usp=sharing"
-}
+  resume:
+    "https://drive.google.com/file/d/1q-3jZ8f8plGZi8WA0o4F7MomaC-CfXfl/view?usp=sharing",
+};
