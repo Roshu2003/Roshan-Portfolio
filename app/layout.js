d5f4d4b6335b2f41cfc3944b2c7c8e86bfb7d5eb
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-04-17 14:36:41 +0530

    change the meta data

--- app/layout.js
+++ app/layout.js
@@ -10,17 +10,17 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Saheb Ansari - Software Developer",
+  title: "Portfolio of Roshan - Software Developer",
   description:
-    "This is the portfolio of Saheb Ansari. I am a Android  developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Roshan. I am a software developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         <ToastContainer />
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <main className='min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white'>
           <Navbar />
           {children}
           <ScrollToTop />
