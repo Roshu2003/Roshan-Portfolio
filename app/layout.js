a8594f32f8559134b83466e94dde533ade97e00d
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-04-17 17:42:48 +0530

    added the favicon

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
   title: "Portfolio of Roshan - Software Developer",
   description:
     "This is the portfolio of Roshan. I am a software developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
