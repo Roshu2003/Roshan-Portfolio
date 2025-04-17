f16b363f66e7f6fac7c1025e71645ecf540c3a4a
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-04-17 19:14:51 +0530

    resolve error

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         <main className='min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white'>
           <Navbar />
           {children}
-          <ScrollToTop />
+          {/* <ScrollToTop /> */}
         </main>
         <Footer />
       </body>
